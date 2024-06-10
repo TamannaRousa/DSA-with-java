e9727aa806186d710f0d1067b05595d24c1ebf60
TamannaRousa <tamanna.rousa.cs25@iilmcet.ac.in>
2024-06-10 21:28:51 +0530

    Height Checker | Sorting

---
+++ HeightChecker.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int count=0;
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+
+        for(int i=0;i<heights.length;i++){
+            if(heights[i] != expected[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
